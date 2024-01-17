8d4b8071bf9a34595309445acce5be532feb7d12
itsgichia <gichiamuiruri@gmail.com>
2024-01-17 18:13:36 +0300

    second commit of index.js

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 // Event listener to execute code when the DOM (document) is fully loaded
 document.addEventListener('DOMContentLoaded', () => {
-    // retieve the container elemen to display meals
+    // Retrieve the container elemen to display meals
     const mealListContainer = document.getElementById('mealList');
+
+    // Fetch meals from the API
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=Malaysian')
+        .then(response => response.json()) // parse the JSON response
+        .then(data => {
+            console.log('data from api:', data); // log data to the console for debugging
+
+            // Check if meals are available in the data
+            if(data.meals && data.meals.length > 0){
+                // Display each meal by creating HTML elements dynamically
+                data.meals.forEach(meal => {
+                    const mealCard = createMealCard(meal);
+                    mealListContainer.appendChild(mealCard);
+                });
+            } 
+            else() =>{
+                // Display a message if no meals are found
+                mealListContainer.innerHTML = '<p> No meals found </p>';
+            }
+        })
+        
 })
