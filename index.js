ee3301865f7f622646ba279c7653dc56ae71077a
itsgichia <gichiamuiruri@gmail.com>
2024-01-17 17:43:19 +0300

    first commit of index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// Event listener to execute code when the DOM (document) is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    // retieve the container elemen to display meals
+    const mealListContainer = document.getElementById('mealList');
+})
